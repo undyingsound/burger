24afadfb100e5fc15f9e9275c89a8489e6ed2e61
Mike Reza <mikerza@gmail.com>
2018-11-13 21:01:47 -0600

    heroku fix

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		host: 'Localhost',
-		user: 'root',
-		password: 'Berkowitz5!',
-		database: 'burgers_db'
+		host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'seotfqxeh7eangx3',
+		password: 'df8xcbj4kj8q5qd8',
+		database: 'kzsm9dicyuu8rh75'
 	})
 }
 
